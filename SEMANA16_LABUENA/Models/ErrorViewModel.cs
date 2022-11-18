00c80f429231c9aecc72e065b0e23d144c4c4aaf
Alumnos <Alumnos@LABC2PC16>
2022-11-18 11:46:23 -0600

    Agregar archivos de proyecto.

---
+++ SEMANA16_LABUENA/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SEMANA16_LABUENA.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ SEMANA16_LABUENA/semana-16-2/Controllers/HelloWordController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+
+namespace semana_16_2.Controllers
+{
+    public class HelloWordController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Bienvenida(string name, int numTimes = 1)
+        {
+            ViewData["name"] = "Hola" + name;
+            ViewData["numTimes"] = numTimes;
+            return View();
+        } 
+
+        public string Parametros(string name, int numTimes = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hola {name}, su numero de intento es: {numTimes}");
+        }
+    }
+}
